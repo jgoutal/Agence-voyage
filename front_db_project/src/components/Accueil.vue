e224b6140cc4fbbfea49ba49581091a9c9ce2531
jgoutal <joadgoutal13@gmail.com>
2021-11-28 19:22:26 +0100

    create branch

--- front_db_project/src/components/Accueil.vue
+++ front_db_project/src/components/Accueil.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <h1> 
-            Bienvenue sur la page de gestion de la base de donnée "Agence de voyage SNCF".
+        <h1>Bienvenue sur la page de gestion de la base de donnée "Agence de voyage SNCF".</h1>
+        <p class="msg"> 
             Vous pouvez accéder à la liste de tous les billets, cliquer sur un billet pour voir ses informations, 
             chercher un billet par gare d'arrivée, ajouter des billets à la vente.
-        </h1>
+
+            En cours de developpement
+        </p>
 
 
     </div>
@@ -18,5 +20,9 @@ export default {
 
 
 <style>
+.msg{
+font-size: 12px
+
+}
 
 </style>
