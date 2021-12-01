a8d7eca376f024d62070095c32d2b7589bce0315
Numa Gout <numagout@gmail.com>
2021-12-01 11:05:30 +0100

    [List-Billet] Link to billet page

--- model/billet-model.js
+++ model/billet-model.js
@@ -2,7 +2,7 @@ let connection = require("../config/db-config")
 let Billet = require("../controllers/billet")
 
 exports.getBillet = (VilleDepart, VilleDestination, date, cb) => {
-    connection.query('SELECT B.HeureDepart, B.HeureArrivee, B.Prix, B.Train, B.VilleDestination, B.VilleDepart, G1.Name GareDepart, G2.Name GareDestination, TIMEDIFF(TIMESTAMP(B.DateArrivee,B.HeureArrivee),TIMESTAMP(B.DateDepart,B.HeureDepart)) Duree FROM Billet B, Gares G1, Gares G2 WHERE VilleDepart=? AND VilleDestination=? AND DateDepart=? AND G1.id = B.GareDepart AND G2.id = B.GareArrivee', [VilleDepart, VilleDestination, date], (err, res, _field) => {
+    connection.query('SELECT B.idBillet, B.HeureDepart, B.HeureArrivee, B.Prix, B.Train, B.VilleDestination, B.VilleDepart, G1.Name GareDepart, G2.Name GareDestination, TIMEDIFF(TIMESTAMP(B.DateArrivee,B.HeureArrivee),TIMESTAMP(B.DateDepart,B.HeureDepart)) Duree FROM Billet B, Gares G1, Gares G2 WHERE VilleDepart=? AND VilleDestination=? AND DateDepart=? AND G1.id = B.GareDepart AND G2.id = B.GareArrivee', [VilleDepart, VilleDestination, date], (err, res, _field) => {
         if (err) throw err
         cb(res)
     })
